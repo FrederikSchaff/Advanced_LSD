655f78268c01f47a88c64be9d23492d322bc7902
Kyaw <Kyaw.Oo@ruhr-uni-bochum.de>
2018-07-05 14:38:12 +0200

    for loop

--- Advanced_LSD/gis_backend/gis.h
+++ Advanced_LSD/gis_backend/gis.h
@@ -227,7 +227,7 @@ ext_gis_rsearch temp_gis_search_obj;
 #define GIS_CYCLE_NEIGHBOURSS(gis_obj,Xobj,x,y,radius,type) \
   temp_gis_search_obj = ext_gis_rsearch(P_EXTS(gis_obj,ext_gis),x,y,radius,type); \
   PLOG("\n There are %i valid objects to scan.",temp_gis_search_obj.valid_objects.size()); \
-  for (Xobj=temp_gis_search_obj.next();Xobj!=NULL;Xobj=temp_gis_search_obj.next())
+  for (Xobj=temp_gis_search_obj.next();temp_gis_search_obj.it_valid != temp_gis_search_obj.valid_objects.end();Xobj=temp_gis_search_obj.next())
 
 #define GIS_CYCLE_NEIGHBOURS_SIMPLES(gis_obj,Xobj,x,y,radius) \
   GIS_CYCLE_NEIGHBOURSS(gis_obj,Xobj,x,y,radius,0)
