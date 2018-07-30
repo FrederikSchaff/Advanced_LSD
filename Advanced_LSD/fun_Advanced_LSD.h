1053ad2f102b37ed88b0408c1960bf7ae13aaca7
Frederik Schaff <frederik.schaff@FernUni-Hagen.de>
2018-07-30 10:23:18 +0200

    fix loading lsd equation files only if module selected.

--- Advanced_LSD/fun_Advanced_LSD.h
+++ Advanced_LSD/fun_Advanced_LSD.h
@@ -1,4 +1,10 @@
 /* Include the LSD functions */
-#include "ABMAT/ABMAT_LSD.cpp"   //load the ABMAT equations
-#include "pop_backend/fun_pop.cpp"    //load population backend EQUATIONS
-#include "gis_backend/fun_gis.cpp"    //load gis backend EQUATIONS
+#ifdef MODULE_ABMAT
+  #include "ABMAT/ABMAT_LSD.cpp"   //load the ABMAT equations
+#endif
+#ifdef MODULE_POPULATION
+  #include "pop_backend/fun_pop.cpp"    //load population backend EQUATIONS
+#endif
+#ifdef MODULE_GEOGRAPHY
+  #include "gis_backend/fun_gis.cpp"    //load gis backend EQUATIONS
+#endif
